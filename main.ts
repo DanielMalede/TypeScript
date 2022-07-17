f62c9f91a86f2db0e8a16fd52ce2228d9848735f
DanielMalede <danielmalede@gmail.com>
2022-07-17 15:22:44 +0300

    .

---
+++ main.ts
@@ -0,0 +1,37 @@
+let usernumber = document.createElement("input")
+usernumber.append()
+function getUserNum(usernumber: number):void {
+
+    if (usernumber > 10) {
+        console.log("greater then 10");
+    }
+    else {
+        console.log("less then 10");
+
+    }
+}
+
+function getNumberFromUser(usernumber: number):void {
+    if (usernumber > 10) {
+        console.log("greater then 10");
+    }
+    else if (usernumber < 10) {
+        console.log("less then 10");
+    }
+    else if (usernumber == 10) {
+        console.log("equal to 10");
+    }
+}
+
+function cample(first:number,second:number):void {
+    if (first > second) {
+        console.log(first);
+    }
+    else if (second > first) {
+        console.log(second);
+    }
+    else if (second == first) {
+        console.log("values are equal");
+    }
+}
+
