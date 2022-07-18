be028fc78b07f03237b9488f3caccd40e4bdb4ec
DanielMalede <danielmalede@gmail.com>
2022-07-18 14:55:13 +0300

    .

--- main.ts
+++ main.ts
@@ -1,40 +1,109 @@
-let usernumber = document.createElement("input")
-usernumber.append()
-function getUserNum(usernumber: number):void {
+// let usernumber = document.createElement("input")
+// usernumber.append()
+// function getUserNum(usernumber: number):void {
 
-    if (usernumber > 10) {
-        console.log("greater then 10");
-    }
-    else {
-        console.log("less then 10");
+//     if (usernumber > 10) {
+//         console.log("greater then 10");
+//     }
+//     else {
+//         console.log("less then 10");
 
-    }
-}
+//     }
+// }
 
-function getNumberFromUser(usernumber: number):void {
-    if (usernumber > 10) {
-        console.log("greater then 10");
-    }
-    else if (usernumber < 10) {
-        console.log("less then 10");
-    }
-    else if (usernumber == 10) {
-        console.log("equal to 10");
-    }
-}
+// function getNumberFromUser(usernumber: number):void {
+//     if (usernumber > 10) {
+//         console.log("greater then 10");
+//     }
+//     else if (usernumber < 10) {
+//         console.log("less then 10");
+//     }
+//     else if (usernumber == 10) {
+//         console.log("equal to 10");
+//     }
+// }
 
-function cample(first:number,second:number):void {
-    if (first > second) {
-        console.log(first);
-    }
-    else if (second > first) {
-        console.log(second);
-    }
-    else if (second == first) {
-        console.log("values are equal");
-    }
-}
+// function cample(first:number,second:number):void {
+//     if (first > second) {
+//         console.log(first);
+//     }
+//     else if (second > first) {
+//         console.log(second);
+//     }
+//     else if (second == first) {
+//         console.log("values are equal");
+//     }
+// }
 
-array.forEach(element => {
-    
-});
+// let usernumber = document.createElement("input")
+// usernumber.append()
+// function getUserNum(usernumber: number):void {
+
+//     if (usernumber > 10) {
+//         console.log("greater then 10");
+//     }
+//     else {
+//         console.log("less then 10");
+
+//     }
+// }
+
+// function getNumberFromUser(usernumber: number):void {
+//     if (usernumber > 10) {
+//         console.log("greater then 10");
+//     }
+//     else if (usernumber < 10) {
+//         console.log("less then 10");
+//     }
+//     else if (usernumber == 10) {
+//         console.log("equal to 10");
+//     }
+// }
+
+// function cample(first:number,second:number):void {
+//     if (first > second) {
+//         console.log(first);
+//     }
+//     else if (second > first) {
+//         console.log(second);
+//     }
+//     else if (second == first) {
+//         console.log("values are equal");
+//     }
+// }
+
+// function returnMinNum(num1:number,num2:number):number{
+//    if(num1<num2){
+//      return num1
+//   }
+//   else{
+//     return num2
+//   }
+// }
+// // console.log(returnMinNum(6,8)) 
+
+// function chackTreeNumbers(num1:number,num2:number,num3:number):number{
+//   return Math.min(num1,num2,num3)
+// }
+// console.log(chackTreeNumbers(10,5,7)) 
+
+// function chackAvregeNum(num1:number,num2:number,num3:number):number{
+//   return num1+num2+num3/3
+// }
+// // console.log(chackAvregeNum(5,1,8))
+
+// function chackFourNum(num1:number,num2:number,num3:number,num4:number):number{
+//   return num1+num2+num3+num4
+// }
+// // console.log(chackFourNum(4,2,4,2))
+
+// function returnMinNumbersArray(numArray:number[]):number{
+//     let minNum = numArray[0]
+//     numArray.forEach(item => {
+//       if(item<minNum)
+//         minNum=item
+//     })
+//     return minNum
+//   }
+//   console.log(returnMinNumbersArray([4,4,3,8,1,7,15]))
+  
